3302d775afed3bfe88291adc47fc6b560b944f49
Sharvirala-Sai-Advika <saiadvikasharvirala@gmail.com>
2025-09-20 19:04:52 +0530

    Update ParkingMeterCoins.java

--- TG-JAVA-555-Sai Advika Arrays and Strings/ParkingMeterCoins.java
+++ TG-JAVA-555-Sai Advika Arrays and Strings/ParkingMeterCoins.java
@@ -1,26 +1,14 @@
-public class ParkingMeterCoins {
+import java.util.Arrays;
+
+public class ParkingMeterCoinsSimple {
     public static void main(String[] args) {
         int[] meters = {120, 90, 300, 150, 200, 75, 400, 50, 180, 220, 310, 95};
 
-        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE, third = Integer.MIN_VALUE;
-        int firstIndex = -1, secondIndex = -1, thirdIndex = -1;
-
-        for (int i = 0; i < meters.length; i++) {
-            if (meters[i] > first) {
-                third = second; thirdIndex = secondIndex;
-                second = first; secondIndex = firstIndex;
-                first = meters[i]; firstIndex = i;
-            } else if (meters[i] > second) {
-                third = second; thirdIndex = secondIndex;
-                second = meters[i]; secondIndex = i;
-            } else if (meters[i] > third) {
-                third = meters[i]; thirdIndex = i;
-            }
-        }
-
+        Arrays.sort(meters); 
+        int n = meters.length;
         System.out.println("Top 3 meters with most coins:");
-        System.out.println("1. Meter " + (firstIndex + 1) + " with " + first + " coins");
-        System.out.println("2. Meter " + (secondIndex + 1) + " with " + second + " coins");
-        System.out.println("3. Meter " + (thirdIndex + 1) + " with " + third + " coins");
+        System.out.println(meters[n - 1]); 
+        System.out.println(meters[n - 2]); 
+        System.out.println(meters[n - 3]); 
     }
 }
